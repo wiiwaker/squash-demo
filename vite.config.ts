c2c8e8b895896caaa6b34608f6d458bcc28c3610
MengQi Li <mengqili@MengQis-MacBook-Pro.local>
2025-12-15 16:16:07 +0800

    update project

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, ".", "");
+
 	return {
+		plugins: [
+			react(),
+			tailwindcss(), // ⭐⭐⭐ 关键
+		],
 		server: {
 			port: 3000,
 			host: "0.0.0.0",
 		},
-		plugins: [react()],
 		define: {
 			"process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
 			"process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
@@ -17,12 +22,6 @@ export default defineConfig(({ mode }) => {
 		resolve: {
 			alias: {
 				"@": path.resolve(__dirname, "."),
-			},
-		},
-		build: {
-			outDir: "dist", // 指定构建目录
-			rollupOptions: {
-				input: path.resolve(__dirname, "index.html"), // 指定入口
 			},
 		},
 	};
